79f2dbcf78aea513b61ed4ea9dc14810b8058447
derickk01 <edduardodeerick123@gmail.com>
2025-07-24 17:53:05 -0300

    Adicionado comentários

--- etl/load.py
+++ etl/load.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from config.settings import caminho_output
 
+# Função para carregar em um arquivo csv
 def carregar_dados(df, output):
-    df.to_csv("output/paises.csv", index=False)
+    df.to_csv(caminho_output, index=False)
