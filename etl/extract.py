094b387a11a5673219224f10041a2ebce94c010f
derickk01 <edduardodeerick123@gmail.com>
2025-07-24 17:51:15 -0300

    Adicionando comentários

--- etl/extract.py
+++ etl/extract.py
@@ -1,15 +1,21 @@
 import pandas as pd
 import requests 
+from config.settings import URL_API
+
 
 def extrair_paises():
-    url = 'https://servicodados.ibge.gov.br/api/v1/localidades/paises' 
+    # Extraindo os dados da API do IBGE
+    url = URL_API
     resultado = requests.get(url)
+    
+    # Verificando se o status code retorna diferente de 200, se isso acontecer, retorna um erro na API
     if resultado.status_code != 200:
         raise Exception(f"Erro na API: {resultado.status_code}")
+    
+    # Colocando o resultado da extração em um json
     dados = resultado.json()
+
+    # Criando um DataFrame
     df = pd.DataFrame(dados)
     return df
 
-if __name__ == "__main__":
-    df = extrair_paises()
-    print(df.head())
