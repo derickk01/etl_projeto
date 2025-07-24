6e3f4c5496369b2523e83d7838adbb425a3da87f
derickk01 <edduardodeerick123@gmail.com>
2025-07-24 17:32:58 -0300

    Alterando os parâmetros para chamar carregar_dados

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from etl.extract import extrair_paises
 from etl.transform import transformar_dados
-
+from etl.load import carregar_dados
 if __name__ == "__main__":
+    # Extraindo
+    df_extract = extrair_paises()
 
-    dados = extrair_paises()
-
-    df = transformar_dados(dados)
-
-    print(df.head())
+    # Transformando
+    df_transform = transformar_dados(df_extract)
+    
+    #Carregando
+    carregar_dados(df_transform, "output/paises.csv")
+    
 
